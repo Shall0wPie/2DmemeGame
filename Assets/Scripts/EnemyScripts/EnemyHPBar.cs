a1c3220db11c30ef399e0cc9b12937197cd647bc
obladaun12 <vfrcbvlehfxbyf@gmail.com>
2020-01-12 01:22:02 +0500

    eblya s scale

---
+++ Assets/Scripts/EnemyScripts/EnemyHPBar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHPBar : MonoBehaviour
+{
+    public Image healthBar;
+    private EnemyStats enemystats;
+    private EnemyAnimationControl eminem;
+    public Text HPtext;
+    private Canvas canvas;
+    [SerializeField] private EnemyCombat enccom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        eminem = GetComponentInParent<EnemyAnimationControl>();
+        enemystats = GetComponentInParent<EnemyStats>();
+        canvas = GetComponentInChildren<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        healthBar.fillAmount = enccom.hp / enemystats.maxHP;
+        HPtext.text = enccom.hp.ToString();
+        
+        
+            Vector2 iceCube = transform.parent.lossyScale;
+            
+            canvas.transform.localScale = iceCube;
+        
+        
+        
+    }
+}
