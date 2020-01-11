b694e7a22142b38be376c58582b3205f2eb5d326
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-01-12 01:59:39 +0500

    HP bar numbers fix

---
+++ Assets/Scripts/UI/EnemyHPBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHPBar : MonoBehaviour
+{
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Text HPtext;
+    [SerializeField] private EnemyCombat combat;
+    private EnemyStats stats;
+    private Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stats = GetComponent<EnemyStats>();
+        canvas = GetComponentInChildren<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = combat.hp / stats.maxHP;
+        HPtext.text = combat.hp.ToString();
+
+        Vector2 iceCube = transform.localScale;
+        canvas.transform.localScale = iceCube;
+    }
+}
---
+++ Assets/Scripts/UI/HPBarScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBarScript : MonoBehaviour
+{
+    public Image healthBar;
+    public Text HPtext;
+    private PlayerCombat combat;
+    private PlayerStats stats;
+
+    void Start()
+    {
+        combat = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<PlayerCombat>();
+        stats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+    }
+
+    void Update()
+    {
+
+        healthBar.fillAmount = combat.hp / stats.maxHP;
+        HPtext.text = combat.hp.ToString();
+        if ((combat.hp * 100 / stats.maxHP) > 60)
+        {
+            HPtext.color = Color.green;
+        }
+        if ((combat.hp * 100 / stats.maxHP) > 20 && (combat.hp * 100 / stats.maxHP) <= 60)
+        {
+            HPtext.color = Color.yellow;
+        }
+        if ((combat.hp * 100 / stats.maxHP) <= 20)
+        {
+            HPtext.color = Color.red;
+        }
+    }
+}
