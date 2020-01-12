579f1625cadb67666057db3476a6502150acddf6
obladaun12 <vfrcbvlehfxbyf@gmail.com>
2020-01-12 13:30:04 +0500

    some sounds and phase

--- Assets/Scripts/EnemyScripts/MoskvinScripts/MoskvinCombat.cs
+++ Assets/Scripts/EnemyScripts/MoskvinScripts/MoskvinCombat.cs
@@ -10,14 +10,15 @@ public class MoskvinCombat : EnemyCombat
     [Range(0f, 10f)] public float TpRange;
     private bool isInRange = false;
     private int prevPoint = -1;
-
+    //Vector2[] point1;   
     private float tamponTimeStamp = 0;
     [SerializeField] protected float tamponCooldown = 1f;
     [SerializeField] [Range(0f, 100f)] protected float tamponRange = 1f;
 
+
     protected override void Update()
     {
-        if ((DialogManager.instance.isInDialogue == false))
+        if ((DialogManager.instance.isInDialogue == false) && (hp * 100 / stats.maxHP > 20))
         {
             distance = Vector2.Distance(target.position, transform.position);
             if (distance < TpRange && !isInRange)
@@ -30,6 +31,7 @@ public class MoskvinCombat : EnemyCombat
                 }
                 prevPoint = pos;
                 StartCoroutine(ITeleport(pos));
+        
             }
 
             if (tamponTimeStamp <= Time.time && distance < tamponRange)
@@ -37,7 +39,19 @@ public class MoskvinCombat : EnemyCombat
                 animControl.PlayShoot();
                 StartCoroutine(ShootTampon());
                 tamponTimeStamp = Time.time + tamponCooldown;
+            }           
+        }
+
+        else if ((DialogManager.instance.isInDialogue == false) && (hp * 100 / stats.maxHP < 20))
+        {
+            transform.parent.position = point[7].position;
+            if (tamponTimeStamp <= Time.time && distance < tamponRange)
+            {
+                animControl.PlayShoot();
+                StartCoroutine(ShootTampon());
+                tamponTimeStamp = Time.time + tamponCooldown;
             }
+
         }
     }
 
@@ -76,6 +90,37 @@ public class MoskvinCombat : EnemyCombat
                 break;
         }
     }
+
+    //public IEnumerator ShootTamponPhase()
+    //{
+    //    Transform projectile = Prefabs.instance.projectileTampon;
+    //    projectile.localScale = transform.lossyScale;
+    //    projectile.GetComponent<Projectile>().caster = transform;
+
+    //    Quaternion q = new Quaternion();
+
+
+    //    while (true)
+    //    {
+    //        //force for enemy punch
+    //        yield return null;
+    //        distance = Vector2.Distance(target.position, transform.position);
+    //        if (animControl.renderer.sprite.name.Equals("throw"))
+    //        {
+    //            for (int i = 0; i < point.Length; i++)
+    //            { 
+    //                point1[i] = 
+    //                Vector2[i] dir;
+    //                q.SetFromToRotation(Vector2.up, dir[i]);
+    //                projectile = Instantiate(projectile, transform.position, q);
+    //                projectile.GetComponent<Projectile>().SetVelocityDirection(dir[i]);
+    //            }
+    //            break;
+    //        }
+    //        if (animControl.anim.GetCurrentAnimatorStateInfo(0).IsName("afk"))
+    //            break;
+    //    }
+    //}
 
     private void OnDrawGizmos()
     {
