4dd13b236f11dca40e70e85bdbecdaa22c6ff5b2
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-01-13 21:44:15 +0500

    Musika Normalno dolbit

---
+++ Assets/AuidioManger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuidioManger : MonoBehaviour
+{
+    public AudioClip main;
+    public AudioClip moskvinTheme;
+
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (SaveManager.instance.checkPoint == 3)
+        {
+            if (source.clip != moskvinTheme)
+            {
+                source.clip = moskvinTheme;
+                source.Play();
+            }
+        }
+    }
+}
