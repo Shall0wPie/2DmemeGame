ba1eb9d6a1e36aa7f84841673c74046147ee17a3
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-01-13 21:57:46 +0500

    death sound

--- Assets/AuidioManger.cs
+++ Assets/AuidioManger.cs
@@ -6,7 +6,9 @@ public class AuidioManger : MonoBehaviour
 {
     public AudioClip main;
     public AudioClip moskvinTheme;
+    public AudioClip failSound;
 
+    private Transform player;
     private AudioSource source;
 
     private void Start()
@@ -16,11 +18,31 @@ public class AuidioManger : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (player == null)
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+
+
         if (SaveManager.instance.checkPoint == 3)
         {
             if (source.clip != moskvinTheme)
             {
                 source.clip = moskvinTheme;
+                source.Play();
+            }
+        }
+        else if (player.GetComponent<PlayerStats>().isAlive)
+        {
+            if (source.clip != main)
+            {
+                source.clip = main;
+                source.Play();
+            }
+        }
+        else
+        {
+            if (source.clip != failSound)
+            {
+                source.clip = failSound;
                 source.Play();
             }
         }
