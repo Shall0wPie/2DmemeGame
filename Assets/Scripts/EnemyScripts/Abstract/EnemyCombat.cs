5ed6197ca917e824edd17c142cd7ef95c079b143
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-01-18 17:31:40 +0500

    Keep rollin rollin rollin

--- Assets/Scripts/EnemyScripts/Abstract/EnemyCombat.cs
+++ Assets/Scripts/EnemyScripts/Abstract/EnemyCombat.cs
@@ -13,7 +13,7 @@ public abstract class EnemyCombat : MonoBehaviour
     [SerializeField] protected Vector2 punchForce;
 
     [SerializeField] protected float attackCooldown = 1f;
-    [SerializeField] [Range(0f, 10f)] public float attackRange;
+    [SerializeField] [Range(0f, 30f)] public float attackRange;
     protected float attackTimeStamp = 0;
 
 
@@ -94,7 +94,9 @@ public abstract class EnemyCombat : MonoBehaviour
         // Sets body collider as Triggers to avoid any collisions
         stats.bodyCollider.isTrigger = true;
         // Disables Follow script
-        GetComponentInParent<EnemyControl>().enabled = false;
+        EnemyControl control = GetComponentInParent<EnemyControl>();
+        if (control != null)
+            control.enabled = false;
 
         // Rotates model by 90 degrees
         transform.parent.Rotate(0, 0, -90 * transform.parent.lossyScale.x);
--- Assets/Scripts/EnemyScripts/EnemyStats.cs
+++ Assets/Scripts/EnemyScripts/EnemyStats.cs
@@ -29,9 +29,9 @@ public class EnemyStats : MonoBehaviour
         if (aggroPoint == null)
         {
             //aggroPoint = GameObject.FindGameObjectWithTag("AggroPoints").transform;
-            Transform pp = GameObject.FindGameObjectWithTag("AggroPoints").transform;
+            Transform point = GameObject.FindGameObjectWithTag("AggroPoints").transform;
             Transform pref = Prefabs.instance.point;
-            aggroPoint = Instantiate(pref, spawnPoint, Quaternion.identity, pp);
+            aggroPoint = Instantiate(pref, spawnPoint, Quaternion.identity, point);
             //aggroPoint.position = spawnPoint;
         }
     }
