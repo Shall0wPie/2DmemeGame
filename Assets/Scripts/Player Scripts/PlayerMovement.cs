24d2f146a2565eb4830c589498d076e6057ed966
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-02-08 20:38:23 +0500

    Slope fix

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,16 +1,19 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float jumpCooldown;
-    
+
     [SerializeField] private float runSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Collider2D groundCollider;
+    [SerializeField] private float rayLength;
 
     private new Rigidbody2D rigidbody;
-    
+    RaycastHit2D hit;
+
 
     protected void Awake()
     {
@@ -21,11 +24,24 @@ public class PlayerMovement : MonoBehaviour
     public void Move(float horizontalMove)
     {
         Vector2 velocity = rigidbody.velocity;
-        velocity.x = horizontalMove * runSpeed;
+        if (IsOnSlope())
+        {
+            float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (jumpForce > velocity.y)
+            {
+                velocity.x = Mathf.Cos(slopeAngle * Mathf.Deg2Rad) * horizontalMove * runSpeed;
+                if (hit.normal.x > 0)
+                    horizontalMove *= -1;
+                velocity.y = Mathf.Sin(slopeAngle * Mathf.Deg2Rad) * horizontalMove * runSpeed;
+            }
+        }
+        else
+        {
+            velocity.x = horizontalMove * runSpeed;
+        }
+
         rigidbody.velocity = velocity;
-        
-        // Vector2 targetVelocity = new Vector2(horizontalMove * runSpeed, 0);
-        // rigidbody.position += targetVelocity;
     }
 
     public void Jump()
@@ -38,5 +54,19 @@ public class PlayerMovement : MonoBehaviour
     public bool IsOnGround()
     {
         return groundCollider.IsTouchingLayers(groundLayer);
+    }
+
+    bool IsOnSlope()
+    {
+        if (!IsOnGround())
+            return false;
+        hit = Physics2D.Raycast((Vector2) transform.position, Vector2.down, rayLength, groundLayer);
+        if (hit)
+        {
+            if ((Vector2) hit.normal != Vector2.up)
+                return true;
+        }
+
+        return false;
     }
 }
