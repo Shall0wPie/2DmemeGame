8591ed3bce5e3072b4491c13c005251362c9e497
VirgiliusDagon <36106295+VirgiliusDagon@users.noreply.github.com>
2020-02-09 16:11:56 +0500

    Platform perfcet

--- Assets/Scripts/GameObjects/PlatformMovement.cs
+++ Assets/Scripts/GameObjects/PlatformMovement.cs
@@ -1,90 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformMovement : MonoBehaviour
 {
-    public bool HorizontalMovement;
-    public float moveSpeed;
+    //public bool HorizontalMovement;
+    [SerializeField] private float moveSpeed;
     private bool moveForward;
-    private Transform[] points;
-    Vector3[] pointsStatic;
+    private Rigidbody2D rb;
+    private Collider2D body;
+    private Vector2 currentDest;
+    private Vector2[] pointsStatic;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-        points = GetComponentsInChildren<Transform>();
-        pointsStatic = new Vector3[points.Length];
-        for (int i = 0; i < points.Length; i++)
-        {
-            if (points[i].position != Vector3.zero)
-            {
-                pointsStatic[i] = points[i].position;
-            }
-        }
-        if (HorizontalMovement == true)
-        {
-            if (pointsStatic[1].x < pointsStatic[2].x)
-                moveForward = true;
-            if (pointsStatic[1].x > pointsStatic[2].x)
-                moveForward = false;
-        }
-        if (HorizontalMovement == false)
-        {
-            if (pointsStatic[1].y < pointsStatic[2].y)
-                moveForward = true;
-            if (pointsStatic[1].y > pointsStatic[2].y)
-                moveForward = false;
-        }
-        Vector3 odds = pointsStatic[2] - pointsStatic[1];
-        if (moveForward == true)
-        {
-            pointsStatic[1] = transform.position;
-            pointsStatic[2] = transform.position + odds;
-        }
-        if (moveForward == false)
-        {
-            pointsStatic[2] = transform.position;
-            pointsStatic[1] = transform.position + odds;
-        }
+        rb = GetComponent<Rigidbody2D>();
+        body = GetComponent<Collider2D>();
+        Transform[] points = GetComponentsInChildren<Transform>();
+
+        pointsStatic = new Vector2[2];
+        pointsStatic[0] = points[1].position;
+        pointsStatic[1] = points[2].position;
+
+        transform.position = pointsStatic[0];
+        currentDest = pointsStatic[1];
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Debug.Log(transform.position);
-        Debug.Log(pointsStatic[2]);
-        Debug.Log(moveForward);
-        if (HorizontalMovement == true)
+        if (Vector2.Distance(transform.position, currentDest) <= 1)
         {
-            if (transform.position.x > pointsStatic[2].x)
-                moveForward = false;
-            if (transform.position.x < pointsStatic[1].x)
-                moveForward = true;
+            PeekNext();
         }
-        if (HorizontalMovement == false)
-        {
-            if (transform.position.y > pointsStatic[2].y)
-                moveForward = false;
-            if (transform.position.y < pointsStatic[1].y)
-                moveForward = true;
-        }
+        transform.position = Vector2.MoveTowards(transform.position, currentDest, moveSpeed);
+    }
 
-        if (moveForward==true)
-        {
-            if (HorizontalMovement==true)
-                transform.position = new Vector2(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y);
-            if (HorizontalMovement==false)
-                transform.position = new Vector2(transform.position.x, transform.position.y + moveSpeed * Time.deltaTime);
+    void PeekNext()
+    {
+        if (currentDest == pointsStatic[0])
+            currentDest = pointsStatic[1];
+        else
+            currentDest = pointsStatic[0];
+    }
 
-        }
-        if (moveForward==false)
-        {
-            if (HorizontalMovement==true)
-                transform.position = new Vector2(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y);
-            if (HorizontalMovement==false)
-                transform.position = new Vector2(transform.position.x, transform.position.y - moveSpeed * Time.deltaTime);
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+            other.transform.SetParent(transform);
+    }
 
-        }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Player")
+            other.transform.parent = null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Transform[] points = GetComponentsInChildren<Transform>();
+        Gizmos.DrawWireSphere(points[1].position, 3);
+        Gizmos.DrawWireSphere(points[2].position, 3);
     }
 }
