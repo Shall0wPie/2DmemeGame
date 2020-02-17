6b3d7602b487961846fd5d5175c734d6145f5487
skritayaoshibka <mikhalev54321@mail.ru>
2020-02-17 19:12:58 +0500

    Добавлено окно настроек

---
+++ Assets/Scripts/UI/Settings/SettingsPopup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsPopup : MonoBehaviour
+{
+    //сделать окно активным
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    //сделать окно неактивным
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private SettingsPopup settingsPopup; //поле для выбора окна настроек
+    private bool IsPaused = false; //переменная для проверки, находится ли игра на паузе
+
+    
+    void Start()
+    {
+        settingsPopup.Close();//закрыть окно с настройками при загрузке
+    }
+
+    
+    void Update()
+    {
+        //Обработка нажатия esc
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //если игра не на паузе, открываем окно с настройками, ставим на паузу
+            //иначе, закрываем коно с настройками, убираем паузу
+            if (!IsPaused)
+            {
+                settingsPopup.Open();
+                Time.timeScale = 0;
+            }
+            else
+            {
+                settingsPopup.Close();
+                Time.timeScale = 1f;
+            }
+
+            IsPaused = !IsPaused;
+        }
+    }
+}
